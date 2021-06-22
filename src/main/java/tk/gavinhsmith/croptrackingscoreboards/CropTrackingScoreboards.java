2690b4e5350170853ffba1b0604678901ac1b804
Gavin Smith <39228744+gavinhsmith@users.noreply.github.com>
2021-06-22 14:18:42 -0500

    Update
    - Build Workflow now uses Java 16
    - Made a test event for when the player breaks a block

--- src/main/java/tk/gavinhsmith/croptrackingscoreboards/CropTrackingScoreboards.java
+++ src/main/java/tk/gavinhsmith/croptrackingscoreboards/CropTrackingScoreboards.java
@@ -1,18 +1,25 @@
 package tk.gavinhsmith.croptrackingscoreboards;
 
+import org.bukkit.block.Block;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class CropTrackingScoreboards extends JavaPlugin {
+public final class CropTrackingScoreboards extends JavaPlugin implements Listener {
 
     @Override
     public void onEnable() {
         // Plugin startup logic
         System.out.println("Hola, amigo!");
+        getServer().getPluginManager().registerEvents(this, this);
     }
 
-    @Override
-    public void onDisable() {
-        // Plugin shutdown logic
-        System.out.println("Adios, amigo!");
+    @EventHandler
+    public void onBlockBreak(BlockBreakEvent event) {
+        Block block = event.getBlock();
+        Player player = event.getPlayer();
+        player.sendMessage(block.toString());
     }
 }
